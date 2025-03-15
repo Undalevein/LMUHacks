c0f951f53acf14697919912e217750db8e45dc8e
Atul Venkatesan <atulvenky@gmail.com>
2025-03-15 13:41:42 -0700

    create a request field and filter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
 import App from './App'
-import './index.css'
+import Home from './components/Home'
+import Profile from './components/Profile'
+import CreateRequest from './components/CreateRequest'
 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+
+      <Route path="profile" element={<Profile />} />
+      <Route path="create-request" element={<CreateRequest />} />
+    </Route>
+  )
+)
+
+//https://reactrouter.com/start/declarative/routing
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
