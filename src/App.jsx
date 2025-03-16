33a496009f75cc8d99c065c5fce29eeea24b5265
Atul Venkatesan <atulvenky@gmail.com>
2025-03-15 18:35:27 -0700

    help me please

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import './App.css'
+import RequestFeed from './components/RequestFeed'
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <RequestFeed />
     </div>
   )
 }
